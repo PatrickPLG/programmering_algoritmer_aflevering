04f07454c3aea8a7dab3a1f432a3c890420ba149
PatrickPLG <44425816+PatrickPLG@users.noreply.github.com>
2019-12-06 09:40:57 +0100

    Update Opgaver 1.py

---
+++ Opgaver/Opgaver 1.py
@@ -0,0 +1,10 @@
+forsenCD = input("Have you cheated on your wife? ")
+
+if (forsenCD == "yes"):
+    print("Welcome to the champions club forsenCD")
+    print("Stupid fucking mistakes")
+else:
+    print("Your not welcome in the champions club")
+    
+if (forsenCD == "Two timer"):
+    print("Two time champ")

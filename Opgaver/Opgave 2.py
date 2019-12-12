883895c61a8a020ebdc383426a0081459a9051fa
PatrickPLG <44425816+PatrickPLG@users.noreply.github.com>
2019-12-12 08:59:55 +0100

    Update Opgave 2.py

--- Opgaver/Opgave 2.py
+++ Opgaver/Opgave 2.py
@@ -5,11 +5,51 @@ import time
 import random
 
 def randomSort(A):
-    tal = random.sample(range(1000000), A)
+    tal = A
     tal.sort()
     print(len(tal))
     return tal
 
+for i in range(4):
+    "start" + str(i)
 
-numGen = 100000
-print(randomSort(numGen))
+
+
+start1 = time.time()
+talliste = random.sample(range(1000000), 10000)
+print(talliste)
+print(randomSort(talliste))
+slut1 = time.time()
+
+
+start2 = time.time()
+talliste = random.sample(range(1000000), 20000)
+print(talliste)
+print(randomSort(talliste))
+slut2 = time.time()
+
+
+start3 = time.time()
+talliste = random.sample(range(1000000), 30000)
+print(talliste)
+print(randomSort(talliste))
+slut3 = time.time()
+
+
+start4 = time.time()
+talliste = random.sample(range(1000000), 40000)
+print(talliste)
+print(randomSort(talliste))
+slut4 = time.time()
+
+
+print("")
+print("Resultaterne er:")
+print("1000" + slut1 - start1)
+print("2000" + slut2 - start2)
+print("3000" + slut3 - start3)
+print("4000" + slut4 - start4)
+print("Gennemsnitettet er:")
+slutG = (slut1+slut2+slut3+slut4)/4
+startG = (start1+start2+start3+start4)/4
+print(slutG-startG)

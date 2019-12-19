c0c89558a00a5ba48082510ef7a786373dfc9f85
PatrickPLG <44425816+PatrickPLG@users.noreply.github.com>
2019-12-19 10:05:12 +0100

    Upload

--- Opgaver/Opgave4.py
+++ Opgaver/Opgave4.py
@@ -1,15 +1,28 @@
 def selection_sort():
-    tallist = [2,4,5,1,3,6,9,8,7]
+
+    # Der oprettes et array af tal ved navn tallist
+    # og en variabel n som er længden af tallist
+    tallist = [2, 4, 5, 1, 3, 6, 9, 8, 7]
     n = len(tallist)
 
+    # Der oprettes en løkke i som er længden af tallisten -1
     for i in range(n-1):
+        # mini variablen sættes til at være i
         mini = i
-        
-        for j in range(i+1,n):
+
+        # Der oprettes en løkke j
+        # som længden mellem variablen i +1 til længden af n
+        for j in range(i + 1, n):
+            # Hvis tallist[j] er mindre end tallist[mini]
+            # sættes mini til at være j
             if tallist[j] < tallist[mini]:
                 mini = j
+        # Hvis mini ikke er lig med i
+        # bytter den om på tallist[mini] og tallist[i]
         if mini != i:
             tallist[mini],tallist[i] = tallist[i], tallist[mini]
+    # Resultatet af den sorterede tallist skrives i terminalen
     return tallist
 
+# Printer funktionen selection_sort
 print(selection_sort())
--- Opgaver/Opgave6.py
+++ Opgaver/Opgave6.py
@@ -1,8 +1,35 @@
-#https://www.geeksforgeeks.org/sorting-algorithms/
+# https://www.geeksforgeeks.org/sorting-algorithms/
 
-#Vi har valgt at arbejde med algoritmen IKKE VALGT
+# Vi har valgt at arbejde med algoritmen Gnome sort
 
-#Beskrivelse hvordan den fungere
+# Gnome sort virker ved, at den først tjekker det første tal i tallitsten (position 0) og om der er et tal før det.
+# Da der ikke er et tal før, rykker Gnome sort 1 position frem og tjekker om der er et tal før.
+# Der er et tal før, men da det er mindre end position 1, går den videre.
+# Den går så videre og tjekker for tallet i position 2, tallet i position 2 er så mindre end position 1.
+# Gnome sort bytter så de to tal så det mindre tal nu er på position 1. Derefter tjekker den, om position 1 er mindre en position 0.
+# Position 0 er mindre end position 1. Derfor går koden bare videre til næste tal i rækken, nemlig position 3.
+# Sådan går den frem og tilbage hele tiden, indtil tallisten er blevet sorteret.
+ 
+
+def gnome_sort(A):
+    # opretter en varibel, pos, og sætter en lig med 0
+    pos = 0
+    # tjekker om pos er mindre end længden af længden af A
+    while pos < len(A):
+        # hvis at pos er lig med 0 eller pos er større end eller lig med pos-1
+        # plusser den pos med 1
+        if (pos == 0 or A[pos] >= A[pos-1]):
+            pos = pos + 1
+        else:
+            # bytter om på pos og pos-1 og rykker pos 1 tilbage
+            A[pos], A[pos-1] = A[pos-1], A[pos]
+            pos = pos-1
+    return A
 
 
-#Kode
+# Tallisten som algoritmen skal sortere
+talliste = [4, 312, 12, 312, 3, 1, 23, 1, 5, 2, 312, 31, ]
+# Tallisten inden sortering
+print(talliste)
+# Tallisen efter sortering
+print(gnome_sort(talliste))

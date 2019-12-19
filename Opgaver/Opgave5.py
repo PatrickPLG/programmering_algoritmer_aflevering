edf4b81e1fe2d2775bcb88d3c9d09091056b5167
Klint02 <44356711+Klint02@users.noreply.github.com>
2019-12-19 22:14:48 +0100

    Update Opgave5.py

--- Opgaver/Opgave5.py
+++ Opgaver/Opgave5.py
@@ -24,24 +24,27 @@ def selection_sort(A):
         # Hvis mini ikke er lig med i
         # bytter den om på tallist[mini] og tallist[i]
         if mini != i:
-            tallist[mini],tallist[i] = tallist[i], tallist[mini]
+            tallist[mini], tallist[i] = tallist[i], tallist[mini]
     # Resultatet af den sorterede tallist skrives i terminalen
     return tallist
+
 
 chooseAmount = input("Vil du selv bestemme hvor mange tal der skal sorteres\
  eller skal der køres standard mængde? selv/standard ")
 
-# Starter en forgrening ud fra chooseAmount inputtet 
+# Starter en forgrening ud fra chooseAmount inputtet
 if (chooseAmount == "standard"):
-    # Hvis der er skrevet "standard" 
-    # går den igang med at sorterer 100.000, 200.000, 300.000 og 400.000 tal
+    # Hvis der er skrevet "standard"
+    # går den i gang med at sorterer 100.000, 200.000, 300.000 og 400.000 tal
     for s in range(4):
         # Så udvælger den mængden af tal angivet i numGen variablen
-        # i en liste fra 0-1.000.000 og sætter disse tal til at være i arrayet talliste
+        # i en liste fra 0-1.000.000
+        # og sætter disse tal til at være i arrayet talliste
         talliste = random.sample(range(1000000), numGen)
-        # Så printer den tallisten, hvor den er kørt igennem funktionen selection_sort
+        # Så printer den tallisten,
+        # hvor den er kørt igennem funktionen selection_sort
         print(selection_sort(talliste))
-        # Variablen numGen forhøjes med 100.000 
+        # Variablen numGen forhøjes med 100.000
         numGen = numGen + 1000
         # Variablen trueTime bliver nu lavet og sat til at være StopT-StartT
 elif (chooseAmount == "selv"):
@@ -50,5 +53,6 @@ elif (chooseAmount == "selv"):
     # Så udvælger den mængden af tal angivet i amount variablen
     # i en liste af tal med størrelsen amount * 100
     talliste = random.sample(range(amount * 100), amount)
-    # Så printer den tallisten, hvor den er kørt igennem funktionen selection_sort
+    # Så printer den tallisten,
+    # hvor den er kørt igennem funktionen selection_sort
     print(selection_sort(talliste))

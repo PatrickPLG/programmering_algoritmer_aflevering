6ebfc730e946574ca7a4df9f1b3d5366fc985adc
Klint02 <44356711+Klint02@users.noreply.github.com>
2019-12-19 22:14:54 +0100

    Update Opgave 2.py

--- Opgaver/Opgave 2.py
+++ Opgaver/Opgave 2.py
@@ -20,24 +20,29 @@ def randomSort(A):
     print(len(number))
     # Returnerer number arrayet
     return number
+
+
 chooseAmount = input("Vil du selv bestemme hvor mange tal der skal sorteres\
  eller skal der køres standard mængde? selv/standard ")
 
-# Starter en forgrening ud fra chooseAmount inputtet 
+# Starter en forgrening ud fra chooseAmount inputtet
 if (chooseAmount == "standard"):
-    # Hvis der er skrevet "standard" 
-    # går den igang med at sorterer 100.000, 200.000, 300.000 og 400.000 tal
+    # Hvis der er skrevet "standard"
+    # går den i gang med at sorterer 100.000, 200.000, 300.000 og 400.000 tal
     for i in range(4):
-        # Først sættes StartT variablen til at være programmet da værende uptime
+        # Først sættes StartT variablen til at være
+        # programmets daværende uptime
         StartT = time.time()
         # Så udvælger den mængden af tal angivet i numGen variablen
-        # i en liste fra 0-1.000.000 og sætter disse tal til at være i arrayet talliste
+        # i en liste fra 0-1.000.000
+        # og sætter disse tal til at være i arrayet talliste
         talliste = random.sample(range(1000000), numGen)
-        # Så printer den tallisten, hvor den er kørt igennem funktionen randomSort
+        # Så printer den tallisten,
+        # hvor den er kørt igennem funktionen randomSort
         print(randomSort(talliste))
         # Så laver den variablen StopT som er programmets uptime
         StopT = time.time()
-        # Variablen numGen forhøjes med 100.000 
+        # Variablen numGen forhøjes med 100.000
         numGen = numGen + 100000
         # Variablen trueTime bliver nu lavet og sat til at være StopT-StartT
         trueTime = StopT-StartT
@@ -60,7 +65,7 @@ elif (chooseAmount == "selv"):
     # Variablen trueTime bliver nu sat ind i arrayet times
     times.append(trueTime)
 
-# Printer både arrayet times og summen af times 
+# Printer både arrayet times og summen af times
 print("")
 print(times)
 print("Sammenlagt tid")
